1fdda6508c5a66cac9dd5c8f4d4797d5475eef5f
taukir48 <ashraftaukir@gmail.com>
2024-02-21 01:41:31 +0600

    build gradle file has been changed

--- app/src/main/java/com/taukir/test/interfaces/OnSwipeTouchListener.kt
+++ app/src/main/java/com/taukir/test/interfaces/OnSwipeTouchListener.kt
@@ -29,10 +29,15 @@ open class OnSwipeTouchListener : View.OnTouchListener {
         }
 
 
-        override fun onFling(e1: MotionEvent?, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        override fun onFling(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
             val result = false
             try {
-                val diffY = e2.y - e1!!.y
+                val diffY = e2.y - e1.y
                 val diffX = e2.x - e1.x
                 if (abs(diffX) > abs(diffY)) {
                     if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
